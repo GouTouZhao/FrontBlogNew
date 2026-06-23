b60f0e7479f65675c04157bf9897e4f066f63244
GouTouZhao <zhaoziteng0624@gmail.com>
2026-06-23 23:10:42 +0800

    端口映射更改

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.goutou.space',
-  timeout: 5000,
+  baseURL: 'https://api.goutou.club',
+  timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
